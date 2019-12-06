683de456930e4bf09937417566e5171536787de4
Kudzko <kudkoa@gmail.com>
2019-12-06 19:36:13 +0300

    HiberNate

--- src/main/java/by/andersen/kudko/TestJPA.java
+++ src/main/java/by/andersen/kudko/TestJPA.java
@@ -10,28 +10,28 @@ import javax.persistence.Persistence;
 
 public class TestJPA {
     public static void main(String[] args) {
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Hibernate_JPA");
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        entityManager.getTransaction().begin();
-//
-//        User user = new User();
-//        user.setName("Craig");
-//        user.setSurname("Walls");
-//
-//        entityManager.persist(user);
-//        entityManager.getTransaction().commit();
-//        entityManager.close();
-//        entityManagerFactory.close();
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Hibernate_JPA");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        EntityManager em;
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Category cat = new Category();
-        cat.setTitle("new category");
-        // JUnit обеспечит тест свежим EntityManager'ом
-        em.persist(cat);
-em.getTransaction().commit();
+        User user = new User();
+        user.setName("Craig");
+        user.setSurname("Walls");
+
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
+//        EntityManager em;
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
+//        em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//        Category cat = new Category();
+//        cat.setTitle("new category");
+//        // JUnit обеспечит тест свежим EntityManager'ом
+//        em.persist(cat);
+//        em.getTransaction().commit();
 
     }
 }
